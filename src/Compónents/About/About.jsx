8d6189409bb40adb81a2b32b23818c078290da3d
Otorb <otonielreyes0@gmail.com>
2023-09-08 14:30:04 -0400

    subiendo al dominio

--- src/Compónents/About/About.jsx
+++ src/Compónents/About/About.jsx
@@ -19,50 +19,49 @@ function About() {
             <p className="aboutdetails" style={{ textAlign: "justify" }}>
               Por la gracia de Dios, soy doctora en Ciencias de la Educación,
               egresada de la Universidad Santa María (USM). Mi disertación
-              doctoral se tituló Modelo Epistémico Sistémico para Integrar la
-              Investigación a la Docencia, en la Enseñanza de las Ciencias. 
+              doctoral se tituló “Modelo Epistémico Sistémico para Integrar la
+              Investigación a la Docencia, en la Enseñanza de las Ciencias”.
               Obtuve una Mención Honorifica por la estructura metodológica y
               esmerada labor en la disertación.
               <br /> <br />
-              Realicé una Maestría en Educación: Mención Enseñanza de la Física,
+              Realicé una Maestría en Educación, Mención Enseñanza de la Física,
               de la Universidad Pedagógica Experimental Libertador. La tesis
-              “The Liouville condition and Nambu mechanic” fue publicada en
+              “The Liouville Condition and Nambu Mechanic” fue publicada en
               Journal of Physics A: Mathematical and General: Gen 29 (1996)
               1037-1044. Printed in the U.K.
               <br /> <br />
-              Me gradué como profesora de matemáticas de la Universidad
-              Pedagógica Experimental Libertador (UPEL). Mientras estudiaba fui
-              seleccionada como preparadora en Calculo I y Calculo II.
+              Me gradué como profesora de Matemática de la Universidad
+              Pedagógica Experimental Libertador (UPEL). Mientras estudiaba, fui
+              seleccionada como preparadora en Cálculo I y Cálculo II.
               <br /> <br />
-              Me he desempeñado como profesora de Matemáticas y Física en
-              diferentes instituciones educativas de Venezuela en los niveles
-              secundario (7-12) y universitario. En el nivel secundario además
-              de enseñar, serví como coordinadora de varios eventos:
-              Mini-Olimpiadas de Matemáticas, Exposición de Física, Premio al
-              Mérito para Incrementar el Rendimiento de los Estudiantes y Amor a
-              Su Institución Educativa (lo cual afianza los valores de
-              solidaridad, cooperación, y responsabilidad). He recibido muchos
-              reconocimientos, entre ellos, condecorada con la orden de
-              Francisco de Miranda.
+              Me he desempeñado como profesora de Matemática y Física en
+              diferentes instituciones educativas de Venezuela. En los niveles
+              de Educación Media, además de enseñar, serví como coordinadora de
+              varios eventos: Mini-Olimpiadas de Matemática, Exposición de
+              Física, Premio al Mérito para Incrementar el Rendimiento de los
+              Estudiantes y Amor a su Institución Educativa (lo cual afianza los
+              valores de solidaridad, cooperación, y responsabilidad). He
+              recibido muchos reconocimientos, entre ellos, fui condecorada con
+              la Orden Francisco de Miranda.
               <br /> <br />
               Como docente universitaria, he trabajado en las siguientes
-              instituciones universitarias de Venezuela: Universidad Central de
-              Venezuela, Universidad Pedagógica Experimental Libertador,
-              Universidad Alejandro Humboldt, Universidad Nacional Experimental
-              de las Fuerzas Armadas, Escuela de Hacienda, Escuela Nacional de
-              Administración y Hacienda Pública, Colegio Universitario Francisco
-              de Miranda, y en la Universidad Nueva Esparta. En las diferentes
-              universidades he enseñado los siguientes cursos: Matemática I,
-              Matemática II, Matemática III, Cálculo I, Cálculo II, Matemática
-              para Educación Especial, Juegos y Recursos Educativos de
-              Matemáticas, Metodología de la Investigación, Razonamiento Lógico,
-              Enseñanza de la Matemáticas para futuros docentes integrales,
-              Estadística, Geometría Analítica, Algebra Lineal y Física. Además
-              de ser docente, me he desempeñado como Directora de Investigación
-              a nivel de Pregrado, Postgrado y Doctorado en la Universidad
-              Nacional Experimental de las Fuerzas Armadas (Unefa). Fui jurado
-              de diferentes disertaciones doctorales en la Universidad
-              Santa María (USM).
+              instituciones de Educación Superior del país: Universidad Central
+              de Venezuela, Universidad Pedagógica Experimental Libertador,
+              Universidad Alejandro de Humboldt, Universidad Nacional
+              Experimental de las Fuerzas Armadas, Escuela de Hacienda, Escuela
+              Nacional de Administración y Hacienda Pública, Colegio
+              Universitario Francisco de Miranda, y en la Universidad Nueva
+              Esparta. En las diferentes universidades he enseñado los
+              siguientes cursos: Matemática I, Matemática II, Matemática III,
+              Cálculo I, Cálculo II, Matemática para Educación Especial, Juegos
+              y Recursos Educativos de Matemática, Metodología de la
+              Investigación, Razonamiento Lógico, Enseñanza de la Matemática
+              para Futuros Docentes Integrales, Estadística, Geometría
+              Analítica, Algebra Lineal, y Física. Además de ser docente, me he
+              desempeñado como Directora de Investigación a nivel de Pregrado,
+              Postgrado y Doctorado en la Universidad Nacional Experimental de
+              las Fuerzas Armadas (UNEFA). Fui jurado de diferentes
+              disertaciones doctorales en la Universidad Santa María (USM).
             </p>
 
             <ul className="skilllist" style={{ marginTop: "-50px" }}>
